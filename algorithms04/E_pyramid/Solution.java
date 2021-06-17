cae4a341dec0c2102f51c0a27491027cad689f79
aiegorova <ermilovaalena@gmail.com>
2021-06-17 10:37:46 +0300

    4 E pyramid added arraylist

--- algorithms04/E_pyramid/Solution.java
+++ algorithms04/E_pyramid/Solution.java
@@ -5,12 +5,15 @@ package com.yandexcontest.algorithms04.E_pyramid;
 import java.io.*;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Solution {
 
     public static void main(String[] args) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        int number = 0;
         long height = 0;
 
         try (FileReader fileReader = new FileReader("C:\\\\Java\\input.txt");
@@ -22,14 +25,17 @@ public class Solution {
             for (int i = 0; i < n; i++) {
 
                 int[] data = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                Integer currValue = map.putIfAbsent(data[0], data[1]);
-                if (currValue != null && currValue < data[1])
-                    map.replace(data[0], data[1]);
+                Integer currValue = map.putIfAbsent(data[0], number);
+                if (currValue == null) {
+                    list.add(data[1]);
+                    number++;
+                } else if (list.get(currValue) < data[1])
+                    list.set(currValue, data[1]);
 
             }
 
-            for (int value : map.values()) {
-                height += value;
+            for (int i = 0; i < list.size(); i++) {
+                height += list.get(i);
             }
 
             writer.write("" + height);
