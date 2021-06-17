c2632e5eb096722bbfc029e1046b933cc1aefabc
aiegorova <ermilovaalena@gmail.com>
2021-06-18 00:42:26 +0300

    D_brackets replaced recursion on cycle

--- firsttrial/D_brackets/Solution.java
+++ firsttrial/D_brackets/Solution.java
@@ -9,32 +9,61 @@ public class Solution {
     public static void main(String[] args) {
 
         try (FileReader fileReader = new FileReader("C:\\\\Java\\input.txt");
-            BufferedReader reader = new BufferedReader(fileReader)) {
+            BufferedReader reader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter("C:\\\\Java\\output.txt");
+            BufferedWriter writer = new BufferedWriter(fileWriter)) {
 
             int n = Integer.parseInt(reader.readLine());
 
             char[] chars = new char[2 * n];
-            createString(chars, 0, 0, n);
+
+            long up = 1;
+            long down = 1;
+            for (int i = 1; i <= 2 * n; i++) {
+                if (i <= n) {
+                    chars[i - 1] = '(';
+                    down *= i;
+                } else
+                    chars[i - 1] = ')';
+                if (i > n + 1)
+                    up *= i;
+            }
+
+            writer.write(chars);
+            writer.write('\n');
+
+            int amount = (int) (up / down);
+
+            for (int count = 0; count < amount; count++) {
+
+                int depth = 0;
+                for (int i = 2 * n - 1; i >= 0; --i) {
+
+                    if (chars[i] == '(')
+                        --depth;
+                    else
+                        ++depth;
+                    if (chars[i] == '(' && depth > 0) {
+                        --depth;
+                        int open = (2 * n - i - 1 - depth) / 2;
+                        int close = 2 * n - i - 1 - open;
+                        chars[i] = ')';
+                        for (int j = 1; j <= open; j++) {
+                            chars[i + j] = '(';
+                        }
+                        for (int j = 1; j <= close; j++) {
+                            chars[i + open + j] = ')';
+                        }
+                        writer.write(chars);
+                        writer.write('\n');
+                        break;
+                    }
+
+                }
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
-        }
-
-    }
-
-    public static void createString(char[] chars, int countLeft, int countRight, int countPairs) {
-
-        if (countLeft == countPairs && countRight == countPairs) {
-            System.out.println(chars);
-        } else {
-            if (countLeft < countPairs) {
-                chars[countLeft + countRight] = '(';
-                createString(chars, countLeft + 1, countRight, countPairs);
-            }
-            if (countRight < countLeft) {
-                chars[countLeft + countRight] = ')';
-                createString(chars, countLeft, countRight + 1, countPairs);
-            }
         }
 
     }
